6712390ef994ba3380184e794c875db75b1e21a9
JOSE ANTONIO BOMFIM SANTOS <148293579+JoseAntonioBomfim@users.noreply.github.com>
2024-11-07 12:54:21 +0000

    Operadores Relacionas

---
+++ operadoresrelacionais.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main () {
+    int preco1 = 60;
+    int preco2 = 50;
+
+    if (preco1 < preco2){
+        printf("Preço 2 maior");
+    }else if (preco1==preco2){
+        printf("Os preços são iguais");
+    }else{
+        printf("preço 1 maior");
+    }
+    return 0;
+}
