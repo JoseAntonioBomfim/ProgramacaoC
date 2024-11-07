e05ca51a853a71b7a2f426cf646dd261f04dc690
JOSE ANTONIO BOMFIM SANTOS <148293579+JoseAntonioBomfim@users.noreply.github.com>
2024-11-07 13:36:01 +0000

    Estutura de Decisão

--- operadoresrelacionais.c
+++ operadoresrelacionais.c
@@ -1,15 +1,10 @@
 #include <stdio.h>
 
-int main () {
+int main() {
     int preco1 = 60;
     int preco2 = 50;
 
-    if (preco1 < preco2){
-        printf("Preço 2 maior");
-    }else if (preco1==preco2){
-        printf("Os preços são iguais");
-    }else{
-        printf("preço 1 maior");
-    }
+    printf("Resultado da comparação: %d\n", preco1 < preco2);
+
     return 0;
 }
