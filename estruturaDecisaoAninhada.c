e20a8315086cdba03da7a26877e5f94bc0937330
JOSE ANTONIO BOMFIM SANTOS <148293579+JoseAntonioBomfim@users.noreply.github.com>
2024-11-08 18:18:14 +0000

    Estrutura de Decisão Aninhada if else

--- estruturaDecisaoAninhada.c
+++ estruturaDecisaoAninhada.c
@@ -1,22 +1,28 @@
 #include <stdio.h>
 
 int main() {
-  int idade;
+  int idade, dependentes;
   float renda;
 
   printf("Digite a sua idade: ");
   scanf("%d", &idade);
   printf("Digite a sua renda mensal: ");
   scanf("%f", &renda);
+  printf("Digite o número de dependentes: ");
+  scanf("%d", &dependentes);
 
-  if (idade < 18 || idade > 60) {
-    if (renda < 2000.0) {
-      printf("Você está qualificado para o desconto especial\n");
+  if (idade >= 18 && idade <= 65) {
+    if (renda < 3000.0) {
+      if (dependentes > 2) {
+        printf("Você está qualificado para os benefícios sociais\n");
+      } else {
+        printf("Você não está qualificado para os benefícios devido ao número de dependentes\n");
+      }
     } else {
-      printf("Você não está qualificado para o desconto devido à renda\n");
+      printf("Você não está qualificado para os benefícios devido à renda\n");
     }
   } else {
-    printf("Você não está qualificado para o desconto devido à idade\n");
+    printf("Você não está qualificado para os benefícios devido à idade\n");
   }
 
   return 0;
